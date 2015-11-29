bd62ae4d5d8bf315b93eb87b8b620fbb8e46254e
pengfen <caopeng8787@163.com>
2015-11-29 21:59:56 +0800

    add command file

--- basic/command.php
+++ basic/command.php
@@ -102,4 +102,90 @@
          /usr/bin/系统命令 (普通用户)
          /usr/sbin/系统命令 (超级用户)
     /var 系统相关文档内容
+
+    根目录下的 bin 和sbin usr 目录下的 bin 和 sbin 这四个目录都是用来保存系统命令的
+
+    proc sys 目录不能直接操作 这两个目录保存的是内存的过载点
+
+    链接命令 ln
+    ln -s [原文件] [目标文件]
+    命令英文原意 link
+    功能描述 生成链接文件
+    选项 -s 创建软链接
+
+    硬链接特征
+    拥有相同的 i 节点和存储 block 块 可以看做是同一个文件
+    可通过 i 节点识别
+    不能跨分区
+    不能针对目录使用
+
+    软链接特征
+    类似 windows 快捷方式
+    软链接拥有自己的 i 节点和 block 块 但是数据块中只保存原文件的文件名和 i 节点号 并没有实际的文件数据
+    lrwxrwxrwx l 软链接 (软链接文件权限都为 rwxrwxrwx)
+    修改任意文件 另一个都改变
+    删除原文件 软链接不能使用
+
+    echo 1111 >> /root/cangls
+    cat /tmp/cangls.soft
+    cat /tmp/cangls.hard
+    echo 2222 >> /tmp/cangls.soft
+    cat /tmp/cangls/hard
+    cat /root/cangls
+    echo 3333 >> /tmp/cangls.hard
+    cat /root/cangls
+    cat /tmp/cangls.soft
+    rm -rf /root/cangls
+    ll /tmp
+    touch csb
+    ls
+    ln -s /root/csb /tmp/
+    touch sb
+    ln -s sb /tmp
+    ll /tmp
+
+    文件搜索命令 locate
+    命令搜索命令 whereis 与 which
+    文件搜索命令 find
+    字符串搜索命令 grep
+    find 命令与 grep 命令的区别
+
+    locate 文件名
+    在后台数据库中按文件名搜索 搜索速度更快
+    touch /tmp/cangls
+    updatedb
+    locate cangls 只
+
+    /var/lib/mlocate
+    #locate 命令所搜索的后台数据库
+
+    updatedb 更新数据库
+    /etc/updatedb.conf 配置文件
+    PRUNE_BIND_MOUNTS = "yes"
+    #开启搜索限制
+    PRUNEFS = 
+    #搜索时 不搜索的文件系统
+    PRUNENAMES =
+    #搜索时 不搜索的文件类型
+    PRUNEPATHS =
+    #搜索时 不搜索的路径
+
+    which pwd
+    whereis cd
+    which cd
+
+    echo $PATH (用冒号分隔)
+    PATH 环境变量 定义的是系统搜索命令的路径
+    [root@localhost ~]# echo $PATH
+    /usr/lib/qt-3.3/bin:...
+
+    find [搜索范围] [搜索条件]
+    #搜索文件
+    find / -name install.log
+    #避免大范围搜索 会非常耗费系统资源
+    #find是在系统当中搜索符合条件的文件名 如果需要匹配 使用通配符匹配 通配符是完全匹配
+
+    * 匹配任意内容
+    ? 匹配任意一个字符
+    [] 匹配任意一个中括号内的字符
 */
