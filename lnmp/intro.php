f6ddd283936969a511ca37db7bd525f26bae5437
pengfen <caopeng8787@163.com>
2016-01-02 13:42:33 +0800

    阿里云服务器搭建lnmp环境

--- lnmp/intro.php
+++ lnmp/intro.php
@@ -1,6 +1,25 @@
 <?php
 
 /**
+ali 上搭建环境
+pwd
+mkdir lnmp
+ls
+cd ./lnmp
+wget -c http://soft.vpser.net/lnmp/lnmp1.2-full.tar.gz && tar zxf lnmp1.2-full.tar.gz && cd lnmp1.2-full && ./install.sh lnmp
+yum info mysql #查看 mysql 的版本
+yum info nginx #查看 nginx 的版本
+yum info php   #查看 php 的版本
+
+nginx
+nginx 安装目录 /usr/local/nginx/
+nginx 日志目录 /home/wwwlogs/ (ls /home/wwwlogs/ access.log nginx_error.log)
+nginx 配置文件 /usr/local/nginx/conf/nginx.conf
+
+访问 http://120.24.36.66
+项目目录 ls /home/wwwroot/default
+
+============================================
 ls
 mkdir lnmp
 
